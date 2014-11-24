fcf5a18c9d25e31de74d049a4b72323ef0380cee
ELL_COOL_A <wangchunqibuaa@163.com>
2014-11-24 21:29:32 +0800

    merge

--- js/admin.js
+++ js/admin.js
@@ -1,69 +1,69 @@
 document.write("<script language=javascript src='/js/page_control.js'></script>");
 
 function add_newsoftware() {
-	
-	var name = document.getElementById("nsw_name").value;
-	var manu = document.getElementById("nsw_manu").value;
-	var price = document.getElementById("nsw_price").value;
-	var introd = document.getElementById("nsw_introd").value;
-	var req = document.getElementById("nsw_req").value;
-	var cate = document.getElementById("nsw_cate").value;
-	var image = document.getElementById("nsw_image").value;
 
-	if(name == ""){
-		alert("请填写软件名称");
-		return;
-	}
-	if(manu == ""){
-		alert("请填写软件发行商");
-		return;
-	}
-	if(price == ""){
-		alert("请填写软件价格");
-		return;
-	}
-	if(introd == ""){
-		alert("请填写软件介绍");
-		return;
-	}
-	if(req == ""){
-		alert("请填写软件配置需求");
-		return;
-	}
-	if(image == ""){
-		alert("请上传介绍图片");
-		return;
-	}
+    var name = document.getElementById("nsw_name").value;
+    var manu = document.getElementById("nsw_manu").value;
+    var price = document.getElementById("nsw_price").value;
+    var introd = document.getElementById("nsw_introd").value;
+    var req = document.getElementById("nsw_req").value;
+    var cate = document.getElementById("nsw_cate").value;
+    var image = document.getElementById("nsw_image").value;
 
-	$.ajaxFileUpload
-	(
-		{
-			url:'add_software.php',
-			secureuri:false,
-			fileElementId:'nsw_image',
-			dataType: 'text',
-			data:{s_name:name,s_price:price,s_manufac:manu,s_introd:introd,s_imagepath:image,s_cate:cate,s_requirement:req},
-			success: function (resText)
-			{
-				if(resText == "success")
-				{
-					alert("新增商品成功");
-					//$("#add_new_software").
-				}
-				else
-				{
-					alert(resText);
-				}
-			}
-		});
+    if(name == ""){
+        alert("请填写软件名称");
+        return;
+    }
+    if(manu == ""){
+        alert("请填写软件发行商");
+        return;
+    }
+    if(price == ""){
+        alert("请填写软件价格");
+        return;
+    }
+    if(introd == ""){
+        alert("请填写软件介绍");
+        return;
+    }
+    if(req == ""){
+        alert("请填写软件配置需求");
+        return;
+    }
+    if(image == ""){
+        alert("请上传介绍图片");
+        return;
+    }
+
+    $.ajaxFileUpload
+    (
+        {
+            url:'add_software.php',
+            secureuri:false,
+            fileElementId:'nsw_image',
+            dataType: 'text',
+            data:{s_name:name,s_price:price,s_manufac:manu,s_introd:introd,s_imagepath:image,s_cate:cate,s_requirement:req},
+            success: function (resText)
+            {
+                if(resText == "success")
+                {
+                    alert("新增商品成功");
+                    //$("#add_new_software").
+                }
+                else
+                {
+                    alert(resText);
+                }
+            }
+        });
 }
 
 function delete_nowsoftware(s_id)
 {
-	var msg = "您真的确定要删除吗？";
-	if(confirm(msg)==false)  return;
+    var msg = "您真的确定要删除吗？";
+    if(confirm(msg)==false)  return;
 
-	$.ajax({
+    $.ajax({
         type: "POST",
         url: "delsoftware.php",
         data: {s_id:s_id},
@@ -73,28 +73,38 @@ function delete_nowsoftware(s_id)
                 alert("删除失败");
             else
             {
-            	alert(resText);
-            	var div = "software"+s_id;
-            	$("#"+div).remove();
+                alert(resText);
+                var div = "software"+s_id;
+                $("#"+div).remove();
             }
         }
     });
 }
 
-function jumpto_change(s_id)
+function jumpto_change(btnObj)
 {
-	/*alert(s_id);
-	var div = document.getElementById("software"+s_id);
-	document.getElementById("csw_name").value = div.childNodes[2].innerText;
-	console.log(div.childNodes[2].innerText);*/
+    $(".nav-bar").attr("class","nav-bar");
+    $(".tab-block").attr("class","tab-block");
+    $(document.getElementsByClassName("nav-bar")[2]).attr("class","nav-bar nav-active");
+    $(document.getElementsByClassName("tab-block")[2]).attr("class","tab-block active-tab");
+
+
+    var trObj=$(btnObj.parentNode.parentNode);
+    console.log(trObj.children().first());
+    var td=trObj.children().first();
+    var id=td.text();
+    $("#csw_id").val(td.text());
+    td=td.next();
+    $("#csw_name").val(td.text());
+    td=td.next();
 }
 
 function delete_user(u_id)
 {
-	var msg = "您真的确定要删除吗？";
-	if(confirm(msg)==false)  return;
+    var msg = "您真的确定要删除吗？";
+    if(confirm(msg)==false)  return;
 
-	$.ajax({
+    $.ajax({
         type: "POST",
         url: "deluser.php",
         data: {u_id:u_id},
@@ -104,8 +114,8 @@ function delete_user(u_id)
                 alert("删除失败");
             else
             {
-            	var div = "user"+u_id;
-            	$("#"+div).remove();
+                var div = "user"+u_id;
+                $("#"+div).remove();
             }
         }
     });
@@ -113,10 +123,10 @@ function delete_user(u_id)
 
 function delete_snumber(sn_id)
 {
-	var msg = "您真的确定要删除吗？";
-	if(confirm(msg)==false)  return;
+    var msg = "您真的确定要删除吗？";
+    if(confirm(msg)==false)  return;
 
-	$.ajax({
+    $.ajax({
         type: "POST",
         url: "delsnum.php",
         data: {sn_id:sn_id},
@@ -126,8 +136,8 @@ function delete_snumber(sn_id)
                 alert("删除失败");
             else
             {
-            	var div = "snumber"+sn_id;
-            	$("#"+div).remove();
+                var div = "snumber"+sn_id;
+                $("#"+div).remove();
             }
         }
     });
@@ -135,19 +145,19 @@ function delete_snumber(sn_id)
 
 function add_snnumber()
 {
-	var sn_number = document.getElementById("add-number-number").value;
-	var select = document.getElementById("add-number-software");
-	var index = select.selectedIndex;
-	var s_id = select.options[index].value;
+    var sn_number = document.getElementById("add-number-number").value;
+    var select = document.getElementById("add-number-software");
+    var index = select.selectedIndex;
+    var s_id = select.options[index].value;
     var s_name = select.options[index].text;
 
-	if(sn_number == "")
-	{
-		alert("请输入序列号！");
-		return;
-	}
+    if(sn_number == "")
+    {
+        alert("请输入序列号！");
+        return;
+    }
 
-	$.ajax({
+    $.ajax({
         type: "POST",
         url: "add_snum.php",
         data: {sn_number:sn_number,s_id:s_id},
@@ -157,9 +167,9 @@ function add_snnumber()
                 alert("添加失败");
             else
             {
-            	var innerHTML = "<tr id='snumber"+resText+"'><td>"+resText+"</td><td>"+sn_number+"</td><td>"+s_name+"</td><td><font color='green'>未使用</font></td><td><span class=\"delete-number\" onclick=\"delete_snumber('"+resText+"')\">删除</span></td></tr>";
-            	$("#sn_number_head").before(innerHTML);
-            	alert("添加成功");
+                var innerHTML = "<tr id='snumber"+resText+"'><td>"+resText+"</td><td>"+sn_number+"</td><td>"+s_name+"</td><td><font color='green'>未使用</font></td><td><span class=\"delete-number\" onclick=\"delete_snumber('"+resText+"')\">删除</span></td></tr>";
+                $("#sn_number_head").before(innerHTML);
+                alert("添加成功");
             }
         }
     });
@@ -167,48 +177,48 @@ function add_snnumber()
 
 function changeconfig()
 {
-	var domain = document.getElementById("change-domain-name").value;
-	var remail = document.getElementById("change-receive-email").value;
-	var semail = document.getElementById("change-send-email").value;
-	var semail_password = document.getElementById("change-send-email-password").value;
+    var domain = document.getElementById("change-domain-name").value;
+    var remail = document.getElementById("change-receive-email").value;
+    var semail = document.getElementById("change-send-email").value;
+    var semail_password = document.getElementById("change-send-email-password").value;
 
-	if(domain == "")
-	{
-		alert("域名不能为空");
-		return;
-	}
-	if(remail == "")
-	{
-		alert("接收订单邮箱不能为空");
-		return;
-	}
-	if(semail == "")
-	{
-		alert("发送邮件邮箱不能为空");
-		return;
-	}
-	if(semail_password == "")
-	{
-		alert("发送邮件邮箱密码不能为空");
-		return;
-	}
+    if(domain == "")
+    {
+        alert("域名不能为空");
+        return;
+    }
+    if(remail == "")
+    {
+        alert("接收订单邮箱不能为空");
+        return;
+    }
+    if(semail == "")
+    {
+        alert("发送邮件邮箱不能为空");
+        return;
+    }
+    if(semail_password == "")
+    {
+        alert("发送邮件邮箱密码不能为空");
+        return;
+    }
 
-	$.ajax({
+    $.ajax({
         type: "POST",
         url: "chgconfig.php",
         data: {domain:domain,remail:remail,semail:semail,semail_password:semail_password},
         dataType: "text",
         success: function(resText){
-        	alert(resText);
+            alert(resText);
             if(resText == "error")
                 alert("修改失败");
             else
             {
-            	document.getElementById("change-domain-name").value = domain;
-	            document.getElementById("change-receive-email").value = remail;
-	            document.getElementById("change-send-email").value = semail;
-				document.getElementById("change-send-email-password").value = semail_password;
-            	alert("修改成功");
+                document.getElementById("change-domain-name").value = domain;
+                document.getElementById("change-receive-email").value = remail;
+                document.getElementById("change-send-email").value = semail;
+                document.getElementById("change-send-email-password").value = semail_password;
+                alert("修改成功");
             }
         }
     });
